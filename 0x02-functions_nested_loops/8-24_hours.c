c07e9a5a60f5b8168e3972754607f40cc69b1c28
Ruth Jack <jackruthowana@gmail.com>
2023-03-21 14:03:50 -0700

    Trying

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * jack_baver - Prints every minute of the day of
- * Jack Baver, starting from 00:00 to 23:59.
+ * jack_bauer - Prints every minute of the day of
+ * Jack Baer, starting from 00:00 to 23:59.
  *
  * Return: 0 Success
  */
-void jack_baver(void)
+void jack_baver
 {
 	int hour, minute;
 

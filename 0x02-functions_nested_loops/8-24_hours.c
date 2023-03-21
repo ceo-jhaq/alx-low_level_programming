4f865fbe2d1efab1bf12b4f2d9ff6a1e2c43aa23
Ruth Jack <jackruthowana@gmail.com>
2023-03-21 13:30:01 -0700

    Pushing

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * jack_baver - Prints every minute of the day of
  * Jack Baver, starting from 00:00 to 23:59.
  *
  * Return: 0 Success
  */
-
 void jack_baver(void)
 {
 	int hour, minute;
